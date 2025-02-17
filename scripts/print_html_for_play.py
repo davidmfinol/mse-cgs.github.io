3e4269248f6a45b1cd700a4ff1162b543d6ecf2f
David Finol <davidmfinol@gmail.com>
2025-02-17 01:58:40 -0600

    Add /play

--- scripts/print_html_for_play.py
+++ scripts/print_html_for_play.py
@@ -6,85 +6,176 @@ def generateHTML():
 	output_html_file = "play.html"
 
 	# Start creating the HTML file content
-	html_content = '''<html>
-	<head>
-	  <title>All Sets</title>
-	  <link rel="icon" type="image/x-icon" href="/img/sets.png">
-	  <link rel="stylesheet" href="/resources/mana.css">
-	  <link rel="stylesheet" href="/resources/header.css">
-	</head>
-	<style>
-		@font-face {
-			font-family: 'Beleren Small Caps';
-			src: url('/resources/beleren-caps.ttf');
-		}
-		@font-face {
-			font-family: Beleren;
-			src: url('/resources/beleren.ttf');
-		}
-		body {
-			font-family: 'Helvetica', 'Arial', sans-serif;
-			overscroll-behavior: none;
-			margin: 0px;
-			background-color: #f3f3f3;
-			font-size: 20px;
-			padding-bottom: 30px;
-		}
-		a {
-			text-decoration: none;
-			color: #171717;
-		}
-		.set-table {
-			width: 60%;
-			max-width: 1000px;
-			display: grid;
-			grid-template-columns: 1fr;
-			padding-top: 20px;
-			margin: auto;
-		}
-		.set-header-row {
-			width: 100%;
-			display: grid;
-			grid-template-columns: 0.5fr 2.5fr 0.5fr 0.5fr;
-			gap: 5px;
-			font-weight: bold;
-			padding-bottom: 10px;
-		}
-		.set-row {
-			height: 6em;
-			width: 100%;
-			display: grid;
-			grid-template-columns: 0.5fr 2.5fr 0.5fr 0.5fr;
-			gap: 5px;
-			align-items: center;
-			border-top: 1px solid #171717;
-		}
-		.set-row:hover {
-			background-color: #fafafa;
-		}
-		.set-row:nth-child(2n) {
-		  background-color: #dedede;
-		}
-		.set-row:nth-child(2n):hover {
-			background-color: #e6e6e6;
-		}
-		.set-row img {
-			width: 70%;
-			justify-self: center;
-		}
-		.set-title {
-			font-family: Beleren;
-			letter-spacing: .02em;
-			font-size: 22px;
-		}
-		.set-group {
-			font-family: Beleren Small Caps;
-			text-align: center;
-			font-size: 40px;
-			padding-top: 20px;
-		}
-	</style>
-	<body>
+	html_content = ''''<html>
+<head>
+	<title>Card</title>
+	<link rel="icon" type="image/x-icon" href="/img/favicon.png">
+	<link rel="stylesheet" href="/resources/mana.css">
+	<link rel="stylesheet" href="/resources/header.css">
+</head>
+<style>
+	@font-face {
+		font-family: Beleren;
+		src: url('/resources/beleren.ttf');
+	}
+	body {
+		font-family: 'Helvetica', 'Arial', sans-serif;
+		overscroll-behavior: none;
+		margin: 0px;
+		background-color: #f3f3f3;
+	}
+	a {
+		text-decoration: none;
+	}
+	.banner-container {
+		width: 100%;
+		background-color: #bbbbbb;
+		display: flex;
+		justify-items: center;
+		align-items: center;
+	}
+	.set-banner {
+		font-family: Beleren;
+		display: flex;
+		gap: 30px;
+		align-items: center;
+		justify-items: center;
+		font-size: 40px;
+		color: #171717;
+		margin: auto;
+		padding-top: 10px;
+		padding-bottom: 10px;
+	}
+	.set-banner img {
+		width: 100px;
+	}
+	.image-grid {
+		padding-top: 40px;
+		width: 70%;
+		max-width: 1000px;
+		margin: auto;
+		display: grid;
+		grid-template-columns: minmax(200px, 2fr) minmax(200px, 2.5fr);
+		gap: 30px;
+		padding-bottom: 10px;
+		justify-items: center;
+	}
+	.image-grid img {
+		position: relative;
+	}
+	.card-image {
+		float: left;
+		width: 100%;
+		max-width: 375px;
+		height: auto;
+		display: block;
+	}
+	.card-text {
+		padding-top: 20px;
+		padding-bottom: 20px;
+		background: #fcfcfc;
+		width: 100%;
+		border: 1px solid #d5d9d9;
+		border-top: 3px solid #171717;
+		border-bottom: 3px solid #171717;
+		border-radius: 6px;
+		height: fit-content;
+		min-height: 75%;
+		margin-top: 3%;
+		display: flex;
+		flex-direction: column;
+	}
+	.card-text div {
+		white-space: normal;
+		font-size: 15px;
+		padding-bottom: 10px;
+		padding-left: 12px;
+		padding-right: 12px;
+		line-height: 155%;
+	}
+	.card-text .name-cost {
+		font-weight: bold;
+		font-size: 20px;
+		white-space: pre-wrap;
+	}
+	.card-text .type {
+		font-size: 16px;
+	}
+	.card-text .pt {
+		font-weight: bold;
+	}
+	.designer-notes {
+		padding-top: 10px;
+		border-top: 1px solid #171717;
+	}
+	.card-text br {
+		content: "";
+		display: block;
+		margin-bottom: 5px;
+	}
+	.card-text .printings {
+		margin-top: auto;
+		font-size: 12px;
+		font-weight: bold;
+		padding-bottom: 0px;
+	}
+	.printings {
+		display: none;
+	}
+	.printings a {
+		color: #1338be;
+		text-decoration: none;
+	}
+	.printings a:hover {
+		color: #0492c2;
+	}
+	.img-container {
+		position: relative;
+		align-self: center;
+	}
+	.img-container img {
+		width: 100%;
+		height: auto;
+	}
+	.img-container .btn {
+		background: url('/img/flip.png') no-repeat;
+		background-size: contain;
+		background-position: center;
+		width: 15%;
+		height: 11%;
+		cursor: pointer;
+		border: none;
+		position: absolute;
+		left: 50%;
+		top: 48%;
+		transform: translate(-50%, -50%);
+		opacity: 0.5;
+	}
+	.img-container .btn:hover {
+		background: url('/img/flip-hover.png') no-repeat;
+		background-size: contain;
+		background-position: center;
+	}
+	.img-container .h-img {
+		transform: rotate(90deg);
+		width: 80%;
+	}
+	.img-container a {
+		height: 100%;
+		display: grid;
+		justify-self: center;
+		align-items: center;
+		justify-items: center;
+	}
+	.img-container a > * {
+		grid-row: 1;
+		grid-column: 1;
+	}
+	.hidden {
+		display: none;
+	}
+</style>
+<body>
 		'''
 
 	with open(os.path.join('resources', 'snippets', 'header.txt'), encoding='utf-8-sig') as f:
